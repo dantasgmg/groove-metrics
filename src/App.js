8acd76cf100a2c2c46f905512ce63a04100f0c62
gabrieldmg <gabriel.d.m.g@outlook.com>
2022-04-30 12:02:23 -0300

    exibindo access_token na tela apos login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React, {Component} from 'react';
+import Login from "./Login.js"
+import Dashboard from './DasboardTemp.js';
 
-function App() {
-  return (
-    <div className="App">
-      <a href="http://localhost:8888"> Logar Spotify </a>
-    </div>
-  );
+const code = new URLSearchParams(window.location.search).get('access_token')
+class App extends Component {
+  render() {
+    return( code ? <Dashboard code = {code}/> : <Login/>)
+  }
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+    render(){
+        return(
+            <div className="App">
+            <a href="http://localhost:8888"> Logar Spotify </a>
+            </div>
+        )
+    };
+}
+  
+export default Login;
